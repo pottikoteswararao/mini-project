294d029c6e5802f0c58db89310a63ef7c84607b1
pottikoteswararao <86213638+pottikoteswararao@users.noreply.github.com>
2021-07-13 09:00:48 +0530

    Update test.c

--- 3.Implementation/test/test.c
+++ 3.Implementation/test/test.c
@@ -12,6 +12,10 @@ void test_divi(void);
 void test_cosec1(void);
 void test_gcd(void);
 void test_lcm(void);
+void test_power(void);
+void test_absolute(void);
+void test_sq_root(void);
+void test_cube_root(void);
 /* Required by the unity test framework */
 void setUp(){}
 /* Required by the unity test framework */
@@ -31,6 +35,10 @@ int main()
   RUN_TEST(test_cosec1);
   RUN_TEST(test_gcd);
   RUN_TEST(test_lcm);
+  RUN_TEST(test_power);
+  RUN_TEST(test_absolute);
+  RUN_TEST(test_sq_root);
+  RUN_TEST(test_cube_root);
   
   /* Close the Unity Test Framework */
   return UNITY_END(); 
@@ -72,4 +80,24 @@ void test_lcm(void)
 {
     TEST_ASSERT_EQUAL(6,lcm(2,3));
     TEST_ASSERT_EQUAL(30,lcm(6,10));
+}
+void test_power(void)
+{
+    TEST_ASSERT_EQUAL(8,power(2,3));
+    TEST_ASSERT_EQUAL(1048,power(2,10));
+}
+void test_absolute(void)
+{
+    TEST_ASSERT_EQUAL(6.000,absolute(-6.000));
+    TEST_ASSERT_EQUAL(30.046,absolute(-30.046));
+}
+void test_sq_root(void)
+{
+    TEST_ASSERT_EQUAL(6,sq_root(36));
+    TEST_ASSERT_EQUAL(30,sq_root(900));
+}
+void test_cube_root(void)
+{
+    TEST_ASSERT_EQUAL(2,cube_root(8));
+    TEST_ASSERT_EQUAL(3,cube_root(27));
 }
